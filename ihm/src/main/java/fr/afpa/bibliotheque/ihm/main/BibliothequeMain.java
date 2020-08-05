c32868e56df9102bcb69f8af19235e6d8beca9c7
bilonjea <j.bilong@gmail.com>
2020-08-06 01:31:18 +0200

    add javax

--- ihm/src/main/java/fr/afpa/bibliotheque/ihm/main/BibliothequeMain.java
+++ ihm/src/main/java/fr/afpa/bibliotheque/ihm/main/BibliothequeMain.java
@@ -5,14 +5,14 @@ package fr.afpa.bibliotheque.ihm.main;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import fr.afpa.bibliotheque.business.ExempleLivreBusiness;
 import fr.afpa.bibliotheque.business.TestBusiness;
-import fr.afpa.bibliotheque.business.impl.ExempleLivreBusinessImpl;
 import fr.afpa.bibliotheque.ihm.config.MainConfig;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 import lombok.extern.log4j.Log4j;
 
@@ -21,7 +21,7 @@ import lombok.extern.log4j.Log4j;
  *
  */
 @Log4j
-public class BibliothequeMain {
+public class BibliothequeMain extends Application{
 	
 	
 
@@ -36,17 +36,30 @@ public class BibliothequeMain {
 				+"---------------------------------------");
 	
 		ApplicationContext ctx = new AnnotationConfigApplicationContext(MainConfig.class);
-	
+		
 		TestBusiness test = (TestBusiness) ctx.getBean("testBusiness");
 		test.sayHello();
 		
 		ExempleLivreBusiness livre =  (ExempleLivreBusiness) ctx.getBean("exempleLivreBusinessImpl");
 		
 	
+		 launch(args);
 		
 		
 
 	}
+
+	@Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("scene.fxml"));
+        
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
+        
+        stage.setTitle("JavaFX and Gradle");
+        stage.setScene(scene);
+        stage.show();
+    }
 
 
 }
